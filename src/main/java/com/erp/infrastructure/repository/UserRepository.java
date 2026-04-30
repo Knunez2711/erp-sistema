362e1cdcd5a1dab6c77e73a632075c4c0c68a419
Knunez2711 <tu_correo@gmail.com>
2026-04-29 22:54:38 -0500

    feat: add UserRepository JPA interface

---
+++ src/main/java/com/erp/infrastructure/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.erp.infrastructure.repository;
+
+import com.erp.infrastructure.persistence.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
